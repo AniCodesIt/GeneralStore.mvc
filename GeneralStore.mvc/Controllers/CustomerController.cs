3a539e01ff94bb4fd8845ac99e55fe03f60a6ada
anicodesit <ani.kulka@gmail.com>
2021-03-25 20:51:08 -0400

    Add project files.

---
+++ GeneralStore.mvc/Controllers/CustomerController.cs
@@ -0,0 +1,108 @@
+﻿using GeneralStore.mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.mvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // Add the application DB Context (link to the database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Customer
+        public ActionResult Index()
+        {
+           
+                List<Customer> customerList = _db.Customer.ToList();
+            //Will using full name cause a problem when retrieving an ordered list?
+                List<Customer> orderedList = customerList.OrderBy(customer => customer.FullName).ToList();
+                return View(orderedList);
+            
+            // See below (modifying ApplicationDbContext class)
+            return View(_db.Customers.ToList());
+        }
+        // Get: Customer
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: Customer
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+        // GET : Delete
+        // Product/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+        // GET : Edit
+        // Customer/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+        // POST : Edit// Customer/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(customer).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+        // GET : Details
+        // Product/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _db.Customers.Find(id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+       
+    }
+}
---
+++ GeneralStore.mvc/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.mvc.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId;
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Name;
+        [Required]
+        [Display(Name = "# In Stock")]
+        public int InventoryCount;
+        [Required]
+        public decimal Price;
+        [Required]
+        [Display(Name = "It is food")]
+        public bool IsFood;
+    }
+}
